d99e439496e3f7566836be0c49357781b8d3b70e
Yasser A <r7al.77@gmail.com>
2018-08-26 00:23:54 -0400

    fix var name in config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 RSSOPT_CONFIG = {
-    'opmlLocation': '/Users/yasser/Downloads/feedly-fc3d18b8-63bc-4ce0-a62c-714ef3a650e8-2018-08-24.opml.xml'
+    'OPML_LOCATION': '/path/to/subscriptions.opml'
 }
 FEEDLY_CONFIG = {
     'CLIENT_ID':'add_id',
